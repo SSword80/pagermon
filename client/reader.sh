a7094b6d3c3fec6f27e41bbc20a36a692eff0c06
Sam <s.sword@hotmail.com>
2026-08-21 09:21:24 +1200

    Update radio reader configuration

--- client/reader.sh
+++ client/reader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ######################################################################
 # rtl_fm   : does rtl fm'ish things
-# -d 0101  : device address - omit this if only one dongle
+# -d 0  : device address - omit this if only one dongle
 # -E dc    : removes DC spike
 # -F 0     : does something to do with magic or something
 # -l 15    : squelch setting, tune accordingly
@@ -13,13 +13,13 @@
 # -q		   : ssshhhh
 # -b1		   : blocks a lot of false decodes, set to b0 if you still get some
 # -c 		   : I forget what this does
-# -a POCSAG512 : set to the protocol you're decoding
+# -a POCSAG512 -a POCSAG1200 -a POCSAG2400 -a FLEX : set to the protocol you're decoding
 # -f alpha 	   : omit if you want non-alphanumeric pages
 # -t raw 	   : tells multimon that you're coming from rtl_fm's pipe
 # /dev/stdin   : the place where all the studs come from
 ######################################################################
 # node reader.js : the cool thing that makes magic happen
 ######################################################################
-rtl_fm -d 0101 -E dc -F 0 -l 15 -A fast -f 148.5875M -s22050 - | 
-multimon-ng -q -b1 -c -a POCSAG512 -f alpha -t raw /dev/stdin | 
+rtl_fm -d 0 -E dc -F 0 -l 15 -A fast -f 157.950M -s22050 - | 
+multimon-ng -q -b1 -c -a POCSAG512 -a POCSAG1200 -a POCSAG2400 -a FLEX -f alpha -t raw /dev/stdin | 
 node reader.js
